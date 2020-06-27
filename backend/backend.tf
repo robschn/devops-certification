4803bb504dbb16f861c39a5c7464441e831cf6bb
Robert Schneider <robmschn@gmail.com>
2020-06-27 17:20:59 -0400

    updated config

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,18 +1,44 @@
-resource "kubernetes_pod" "" {
+resource "kubernetes_deployment" "backend" {
   metadata {
-    name = "nginx-example"
+    name = "backend"
     labels = {
-      App = "nginx"
+      App = "backend"
     }
   }
 
   spec {
-    container {
-      image = "nginx:1.7.8"
-      name  = "example"
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "Backend"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Backend"
+        }
+      }
+      spec {
+        container {
+          image = "gcr.io/devops-certification-279819/backend:v1.0"
+          name  = "backend"
 
-      port {
-        container_port = 80
+          port {
+            container_port = 8082
+          }
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
       }
     }
   }
