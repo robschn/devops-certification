077d441f7b5106bf7e78d478b163e25654a7b8d0
Robert Schneider <robmschn@gmail.com>
2020-06-29 15:21:51 -0400

    updated node count

--- cluster.tf
+++ cluster.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "cluster1" {
   name               = "cluster1"
   location           = "us-east1"
-  initial_node_count = 1
+  initial_node_count = 3
 
   network    = "default"
   subnetwork = "default"
