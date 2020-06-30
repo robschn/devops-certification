316e1075458f9ef0f7e079164678f78e40f6b964
Robert Schneider <robmschn@gmail.com>
2020-06-30 16:47:19 -0400

    Update test-server.js

--- frontend/test/test-server.js
+++ frontend/test/test-server.js
@@ -4,36 +4,6 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('GET /', function () {
-  it('responds with home page', function (done) {
-
-    //specify the url to be intercepted
-    nock("http://localhost:8082")
-      //define the method to be intercepted
-      .get('/events')
-      //respond with a OK and the specified JSON response
-      .reply(200, {
-        "status": 200,
-        "events": [
-          { "title": 'an event', "id": 1234, "description": 'something really cool' },
-          { "title": 'another event', id: 5678, "description": 'something even cooler' }
-        ]
-      });
-
-    request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
-        return done();
-      });
-
-
-  });
-
 
   it('should display page when the backend is down', function (done) {
     //specify the url to be intercepted
